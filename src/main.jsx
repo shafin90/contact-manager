2841c1ee6dc940a6102a741bec3837e83039aaee
shafin90 <mashrafiahnam1@gmail.com>
2023-09-09 03:21:16 -0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.css'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Hello world! Project is being started</div>,
   },
 ]);
 
